086085bc280c0cdda2ef527e395e148ba06355a9
Abraham Aigbokhan <abraham.aigbokhan@outlook.com>
2025-05-22 13:52:40 +0100

    Update rectangle_area.py

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,1 +1,11 @@
+# Python script that calculates the area of a rectangle
 
+# variables for the calculation of area of rectangle
+length = 10
+width = 5
+
+# Formular for calculating the interest is (area = length * width)
+area = length * width
+
+# The below prints the area of the rectangle base on the formular and the value of the variables specified above
+print("The area of the rectangle is:", area)
