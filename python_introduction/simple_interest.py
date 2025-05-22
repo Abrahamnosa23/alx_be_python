d1f7d545e28deb14af381a3fa99279943c37c291
Abraham Aigbokhan <abraham.aigbokhan@outlook.com>
2025-05-22 13:33:25 +0100

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,13 @@
 
+# Python script that calculates the simple interest earned on an investment over a period of time
+
+# variables for the calculation of the interest
+principal = 1000
+rate = (0.05)
+time = 3
+
+# Formular for calculating the interest is (I = P * R * T)
+interest = principal * rate * time
+
+# The below prints the interest base on the formular and the value of the variables specified above
+print("The simple interest is:", interest)
