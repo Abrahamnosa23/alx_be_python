4d409b132771f5798cfe1344c481be1d3f922a60
Abraham Aigbokhan <abraham.aigbokhan@outlook.com>
2025-05-22 14:43:18 +0100

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -2,11 +2,9 @@
 
 # variables for the convertion of hours to seconds
 hours = 2
-minutes_in_an_hour = 60
-seconds_in_a_minutes = 60
 
 # Formular for calculating the interest is (seconds = number of hours * number of minutes in an hour * number of seconds in a minute)
-seconds = hours * minutes_in_an_hour * seconds_in_a_minutes
+seconds = hours * 3600
 
 # The below prints the area of the rectangle base on the formular and the value of the variables specified above
 print(hours, "hours is", seconds, "seconds") 
