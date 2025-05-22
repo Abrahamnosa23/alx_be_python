796a2d054e22262cd0a70af64273a0c82cef39d4
Abraham Aigbokhan <abraham.aigbokhan@outlook.com>
2025-05-22 13:21:06 +0100

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,15 @@
+# Below is a basic python script that performs basic arithmetic operations with two predefined numbers
+number1 = 10
+number2 = 5
 
+# Addition Operation
+addition = number1 + number2
+print("Addition of", number1, "and", number2, "is", addition)
+
+# Subtraction Operation
+subtraction = number1 - number2
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+
+# Multiplication Operation
+multiplication = number1 * number2
+print("Multiplication of", number1, "and", number2, "is", multiplication)
